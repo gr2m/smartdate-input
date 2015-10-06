08108ee79c71e877a45039f8e5c0d1c2998f3bb5
zoepage <o.gasidlo@gmail.com>
2015-10-06 10:52:13 +0200

    chore: added standard

--- smartdate-input.js
+++ smartdate-input.js
@@ -1,6 +1,6 @@
 /* global moment */
 (function ($, moment) {
-  'use strict';
+  'use strict'
 
   // SMARTDATE INPUT CLASS DEFINITION
   // ================================
@@ -9,108 +9,107 @@
   //
   //
   var SmartDate = function (el, settings) {
-    var $input;
-    var defaultFormat = 'dddd, MMMM D, YYYY h:mma';
-    var defaultFormatChangeEventType = 'blur';
-    var api = this;
-    var timestamp;
+    var $input
+    var defaultFormat = 'dddd, MMMM D, YYYY h:mma'
+    var defaultFormatChangeEventType = 'blur'
+    var api = this
+    var timestamp
 
-    settings = settings || {};
+    settings = settings || {}
 
     // 1. cache elements for performance reasons and
     // 2. setup event bindings
-    function initialize() {
-      var format;
-      $input = $(el);
+    function initialize () {
+      var format
+      $input = $(el)
 
       // init format
-      format = settings.format || $input.data('format') || defaultFormat;
+      format = settings.format || $input.data('format') || defaultFormat
       if (typeof format === 'function') {
-        settings.format = format(settings);
+        settings.format = format(settings)
       } else {
-        settings.format = format;
+        settings.format = format
       }
-      $input.removeAttr('data-format');
+      $input.removeAttr('data-format')
 
       // init timestamp
-      timestamp = settings.timestamp || $input.data('timestamp');
+      timestamp = settings.timestamp || $input.data('timestamp')
 
       // init event on when to check for changed format timestamp
-      settings.event = settings.event || $input.data('format-event') || defaultFormatChangeEventType;
-      $input.removeAttr('data-format-event');
+      settings.event = settings.event || $input.data('format-event') || defaultFormatChangeEventType
+      $input.removeAttr('data-format-event')
 
-      $input.on(settings.event, handleFormatChangeEvent);
+      $input.on(settings.event, handleFormatChangeEvent)
 
       if (settings.date) {
-        api.set(settings.date, { silent: true });
+        api.set(settings.date, { silent: true })
       }
     }
 
     // PUBLIC API
     // ----------
-    api.set = function set(date, options) {
-      var dateString;
-      var newTimestamp;
+    api.set = function set (date, options) {
+      var dateString
+      var newTimestamp
 
-      if (! options) options = {};
+      if (!options) options = {}
 
       if (!(date instanceof Date) && typeof date === 'object') {
-        return updateSettings(date);
+        return updateSettings(date)
       }
 
-      if (! date) {
-        date = new Date();
+      if (!date) {
+        date = new Date()
       }
       if (typeof date === 'string') {
-        dateString = date;
+        dateString = date
       } else {
         // turning Date Object into string
-        dateString = moment(date).format(settings.format);
+        dateString = moment(date).format(settings.format)
       }
-      $input.val(dateString);
-      settings.format = parseFormat(dateString);
-      newTimestamp = parseTimestamp(dateString);
+      $input.val(dateString)
+      settings.format = parseFormat(dateString)
+      newTimestamp = parseTimestamp(dateString)
       if (newTimestamp !== timestamp) {
-        timestamp = newTimestamp;
+        timestamp = newTimestamp
 
-        if (! options.silent) {
-          $input.trigger('change:timestamp', [newTimestamp]);
+        if (!options.silent) {
+          $input.trigger('change:timestamp', [newTimestamp])
         }
       }
-    };
-    api.setFormat = function setFormat(newFormat) {
-      var val = $input.val();
-      var currentDate;
+    }
+    api.setFormat = function setFormat (newFormat) {
+      var val = $input.val()
+      var currentDate
 
-      if (val) currentDate = moment(val, settings.format).toDate();
+      if (val) currentDate = moment(val, settings.format).toDate()
 
       if (typeof newFormat === 'function') {
-        settings.format = newFormat(settings);
+        settings.format = newFormat(settings)
       } else {
-        settings.format = newFormat;
+        settings.format = newFormat
       }
 
-      if (val) api.set(currentDate);
-    };
-    api.setEvent = function setEvent(eventType) {
-      $input.unbind(settings.event, handleFormatChangeEvent);
-      settings.event = eventType;
-      $input.on(settings.event, handleFormatChangeEvent);
-    };
-    api.getFormat = function(callback) {
-      callback(settings.format);
-    };
-    api.get = function(callback) {
-      var val = $input.val();
-      var currentDate;
+      if (val) api.set(currentDate)
+    }
+    api.setEvent = function setEvent (eventType) {
+      $input.unbind(settings.event, handleFormatChangeEvent)
+      settings.event = eventType
+      $input.on(settings.event, handleFormatChangeEvent)
+    }
+    api.getFormat = function (callback) {
+      callback(settings.format)
+    }
+    api.get = function (callback) {
+      var val = $input.val()
+      var currentDate
 
-      if (! val) return callback();
+      if (!val) return callback()
 
-      settings.format = parseFormat(val);
-      currentDate = moment(val, settings.format).toDate();
-      callback(currentDate);
-    };
-
+      settings.format = parseFormat(val)
+      currentDate = moment(val, settings.format).toDate()
+      callback(currentDate)
+    }
 
     // Event handlers
     // --------------
@@ -118,21 +117,20 @@
     //
     //
     //
-    function handleFormatChangeEvent( /*event*/ ) {
-      var dateString = $input.val();
-      var newFormat = parseFormat(dateString);
-      var newTimestamp;
+    function handleFormatChangeEvent (/*event*/) {
+      var dateString = $input.val()
+      var newFormat = parseFormat(dateString)
+      var newTimestamp
       if (settings.format !== newFormat) {
-        settings.format = newFormat;
-        $input.trigger('change:format', [newFormat]);
+        settings.format = newFormat
+        $input.trigger('change:format', [newFormat])
       }
-      newTimestamp = parseTimestamp(dateString);
+      newTimestamp = parseTimestamp(dateString)
       if (newTimestamp !== timestamp) {
-        timestamp = newTimestamp;
-        $input.trigger('change:timestamp', [newTimestamp]);
+        timestamp = newTimestamp
+        $input.trigger('change:timestamp', [newTimestamp])
       }
     }
-
 
     // Internal Methods
     // ----------------
@@ -142,7 +140,7 @@
     //
     function updateSettings (newSettings) {
       if (newSettings.format) {
-        api.setFormat(newSettings.format);
+        api.setFormat(newSettings.format)
       }
     }
 
@@ -150,79 +148,77 @@
     //
     //
     function parseFormat (dateString) {
-      var newFormat;
-      var oldFormatCheck;
-      var newFormatCheck;
+      var newFormat
+      var oldFormatCheck
+      var newFormatCheck
 
       // Stop if newFormat is same as old
-      newFormat = moment.parseFormat(dateString);
-      if (newFormat === settings.format) return settings.format;
+      newFormat = moment.parseFormat(dateString)
+      if (newFormat === settings.format) return settings.format
 
       // Stop if old format still fits
-      oldFormatCheck = moment(dateString, settings.format).format(settings.format);
-      if (dateString.toLowerCase() === oldFormatCheck.toLowerCase()) return settings.format;
+      oldFormatCheck = moment(dateString, settings.format).format(settings.format)
+      if (dateString.toLowerCase() === oldFormatCheck.toLowerCase()) return settings.format
 
       // Stop if new format cannot parse dateString correctly
-      newFormatCheck = moment(dateString, newFormat).format(newFormat);
-      if (dateString.toLowerCase() !== newFormatCheck.toLowerCase()) return settings.format;
+      newFormatCheck = moment(dateString, newFormat).format(newFormat)
+      if (dateString.toLowerCase() !== newFormatCheck.toLowerCase()) return settings.format
 
       // sanity check
-      return newFormat;
+      return newFormat
     }
 
     //
     //
     //
     function parseTimestamp (dateString) {
-      var m;
-      if (! settings.format) return timestamp;
-      m = moment(dateString, settings.format, true);
-      if (m.isValid()) return m.format();
-      return timestamp;
+      var m
+      if (!settings.format) return timestamp
+      m = moment(dateString, settings.format, true)
+      if (m.isValid()) return m.format()
+      return timestamp
     }
 
-    initialize();
-  };
-
+    initialize()
+  }
 
   // EDITABLE TABLE PLUGIN DEFINITION
   // ================================
 
   $.fn.smartDate = function (method) {
-    var args = [].slice.call(arguments, 1);
+    var args = [].slice.call(arguments, 1)
     return this.each(function () {
-      var $this = $(this);
-      var api  = $this.data('bs.smartDate');
-      var options;
+      var $this = $(this)
+      var api = $this.data('bs.smartDate')
+      var options
 
       if (typeof method === 'object') {
-        options = method;
+        options = method
       }
       if (!api) {
-        $this.data('bs.smartDate', (api = new SmartDate(this, options)));
+        $this.data('bs.smartDate', (api = new SmartDate(this, options)))
       }
       if (api[method]) {
-        return api[method].apply(this, args);
+        return api[method].apply(this, args)
       }
 
       if (options) {
-        api.set(options);
+        api.set(options)
       }
-    });
-  };
+    })
+  }
 
-  $.fn.smartDate.Constructor = SmartDate;
-
+  $.fn.smartDate.Constructor = SmartDate
 
   // EDITABLE TABLE DATA-API
   // =======================
 
-  $(document).on('input.bs.smartDate.data-api', '[data-smartdate-spy]', function(event) {
-    var $input = $(event.target);
-    event.preventDefault();
-    event.stopImmediatePropagation();
-    $input.removeAttr('data-smartdate-spy');
-    $input.smartDate();
-    $input.trigger( $.Event(event) );
-  });
-})(jQuery, moment);
+  $(document).on('input.bs.smartDate.data-api', '[data-smartdate-spy]', function (event) {
+    var $input = $(event.target)
+    event.preventDefault()
+    event.stopImmediatePropagation()
+    $input.removeAttr('data-smartdate-spy')
+    $input.smartDate()
+    $input.trigger($.Event(event))
+  })
+})(jQuery, moment) // eslint-disable-line
